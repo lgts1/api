d4df78d9815ae77cda13b1c7885230e4a0efec72
lgts1 <168389426+lgts1@users.noreply.github.com>
2024-07-28 17:07:16 -0300

    Update fortunemouse.ts

--- src/controllers/fortune-mouse/fortunemouse.ts
+++ src/controllers/fortune-mouse/fortunemouse.ts
@@ -21,7 +21,7 @@ export default {
          const token = req.body.atk
          const user = await fortunemousefunctions.getuserbyatk(token)
 
-         const jsonformatado = await JSON.parse('{"dt":{"si":{"wp":null,"lw":null,"orl":null,"idr":false,"ir":false,"ist":false,"rc":0,"itw":false,"wc":0,"gwt":0,"fb":null,"ctw":0.0,"pmt":null,"cwc":0,"fstc":null,"pcwc":0,"rwsp":null,"hashr":null,"ml":2,"cs":0.3,"rl":[1,1,1,0,0,0,2,2,2],"sid":"0","psid":"0","st":1,"nst":1,"pf":0,"aw":0.00,"wid":0,"wt":"C","wk":"0_C","wbn":null,"wfg":null,"blb":0.00,"blab":0.00,"bl":100000.00,"tb":0.00,"tbb":0.00,"tw":0.00,"np":0.00,"ocr":null,"mr":null,"ge":null}},"err":null}')
+         const jsonformatado = await JSON.parse('{"dt":{"si":{"wp":{"1":[1,4,7],"2":[0,3,6],"3":[2,5,8],"4":[0,4,8],"5":[2,4,6]},"lw":{"1":10000,"2":10000,"3":10000,"4":10000,"5":10000},"orl":null,"idr":true,"ir":false,"ist":false,"rc":0,"itw":false,"wc":0,"gwt":-1,"fb":null,"ctw":50000,"pmt":null,"cwc":0,"fstc":null,"pcwc":0,"rwsp":false,"hashr":"0:3;0;1#3;0;4#3;0;4#MV#3.0#MT#1#MG#0#","ml":"10","cs":"10","rl":[1,1,1,0,0,0,1,1,1],"sid":"1763628543189646848","psid":"1763628543189646848","st":4,"nst":1,"pf":1,"aw":50000,"wid":0,"wt":"C","wk":"0_C","wbn":null,"wfg":null,"blb":1636610,"blab":1686110,"bl":1686110,"tb":500,"tbb":500,"tw":50000,"np":50000,"ocr":null,"mr":null,"ge":[4,11]}},"err":null}')
 
          res.send({
             dt: {
