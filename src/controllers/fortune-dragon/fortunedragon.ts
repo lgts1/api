1d943eaf9dcc9f8351c293c4ea676f1305484287
lgts1 <168389426+lgts1@users.noreply.github.com>
2024-08-27 22:37:00 -0300

    Update fortunedragon.ts

--- src/controllers/fortune-dragon/fortunedragon.ts
+++ src/controllers/fortune-dragon/fortunedragon.ts
@@ -91,7 +91,7 @@ export default {
       try {
          const user = await fortunedragonfunctions.getuserbyatk(token)
          let bet: number = cs * ml * 5
-         if (req.body.fb) {
+         if (req.body.fb != false) {
             bet = bet * 5
          }
          
@@ -123,7 +123,7 @@ export default {
             }
          }
 
-         if (req.body.fb) {
+         if (req.body.fb != false) {
             resultadospin.result = "ganho"
          }
 
